67afaff7e807a8080bc1bb7fe11068621b949956
Lorena Rodriguez <loreleyro.89@gmail.com>
2022-08-08 19:03:06 -0300

    Probando Rutas

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import { Routes, Route} from "react-router-dom"
+import Detalle from "./paginas/Detalle/Detalle";
+import Error404 from "./paginas/Error404/Error404";
+import Formulario from "./paginas/Formulario/Formulario";
+import Home from "./paginas/Home/Home";
+import PaginaInicial from "./paginas/PaginaInicial/PaginaInicial";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Food</h1>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<PaginaInicial/>}/>
+      <Route exact path="/home" element={<Home/>}/>
+      <Route exact path="/detalle/:id" element={<Detalle/>}/>
+      <Route exact path="/formulario" element={<Formulario/>}/>
+      <Route exact path="*" element={<Error404/>}/>
+    </Routes>
   );
 }
 
